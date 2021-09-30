1f08024988008bdb01aaed7314111752bcd48d8e
bong <qhdgns613@gmail.com>
2021-09-30 16:00:26 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,33 @@
 package dept;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+public class DeptDAO {
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, user, password)
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
