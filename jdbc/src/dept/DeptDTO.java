95c9911121cb62d0b9ccb7325116d5065e9945da
bong <qhdgns613@gmail.com>
2021-09-30 14:37:33 +0900

    "commit 성공"

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,5 +1,18 @@
 package dept;
 
-public class DeptDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DeptDTO {
+	private int deptNo;
+	private String dname;
+	private String loc;
 }
