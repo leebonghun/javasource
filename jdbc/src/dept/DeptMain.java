03b1b724e2beb0ecae7792f5b858907af48fa7e5
bong <qhdgns613@gmail.com>
2021-10-01 11:17:40 +0900

    메뉴화면 구성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,9 +1,18 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
-
+		boolean run = true;
+		Scanner sc = new Scanner(System.in);
+		
+		while(run) {
+			
+			
+		}
+		
 	}
 
 }
